38263410384d836cccaedbd9ce0c9bf54c2c0e00
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-03-02 12:38:23 +0800

    first init, solve problem 35

---
+++ 0035/search_insert_position.py
@@ -0,0 +1,18 @@
+def searchInsert(nums, target):
+    middle = len(nums) // 2
+    left = nums[:middle]
+    mid = nums[middle]
+    right = nums[(middle + 1):]
+    if target == mid: return middle
+    elif target > mid:
+        if right: return middle + 1 + searchInsert(right, target)
+        else: return middle + 1
+    elif target < mid:
+        if left: return searchInsert(left, target)
+        else: return middle
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5]
+    target = 4.7
+    result = searchInsert(nums, target)
+    print(result)
