e84483c7e7c42a7ab590864b24f6c5330358b161
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-03-03 18:28:12 +0800

    solved problem 14

---
+++ 0014/longest_common_prefix.py
@@ -0,0 +1,24 @@
+"""
+Leetcode Problem 0014
+Runtime: 25ms (70.79% faster)
+Memory usage: 13.7mb (64.33% less)
+
+Description:
+Find the longest common prefix string amongst an array of strings
+"""
+
+def longestCommonPrefix(strs):
+    output = strs[0]
+    for i in range(len(strs) - 1):
+        min_len = min(len(strs[i]), len(strs[i + 1]))
+        for j in range(min_len):
+            if strs[i][j] != strs[i + 1][j]:
+                min_len = j
+                break
+        output = output[:min_len]
+    return output
+
+if __name__ == "__main__":
+    strs = ["flower", "flow", "flights"]
+    result = longestCommonPrefix(strs)
+    print(result)
