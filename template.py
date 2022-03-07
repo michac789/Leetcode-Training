048e59e82732d12398a46312ec8afe91a012495c
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-03-07 11:44:35 +0800

    minor modif

--- template.py
+++ template.py
@@ -13,3 +13,18 @@ def func(peri):
 
 if __name__ == "__main__":
     pass
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+    def __str__(self):
+        output = "ListNode: "
+        output = output + str(self.val) + " -> "
+        node = self.next
+        while node:
+            output = output + str(node.val) + " -> "
+            node = node.next
+        output = output[:-4:]
+        return output
